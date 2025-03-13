f3e6cb34f625d3dd75c90f02211beea9652b628f
ManjuKempaiah <136099797+ManjuKempaiah@users.noreply.github.com>
2025-03-13 10:21:10 +0530

    Create tomcatDeploy.groovy

---
+++ vars/tomcatDeploy.groovy
@@ -0,0 +1,7 @@
+def call(ip,creds,warFile) {
+   sshagent([creds]){
+                    sh "scp -o StrictHostKeyChecking=no target/${warFile} ec2-user@${ip}:/opt/tomcat9/webapps"
+                    sh "ssh ec2-user@{ip} /opt/tomcat9/bin/shutdown.sh"
+                    sh "ssh ec2-user@{ip} /opt/tomcat9/bin/startup.sh"
+                }
+}
